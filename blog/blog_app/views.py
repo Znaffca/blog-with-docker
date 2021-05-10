0540d5272d7eeb04e181d3869923f13e7962980f
Grzegorz Salacinski <salacinski.grzegorz@gmail.com>
2021-05-10 23:25:26 +0200

    implement simple search rank using postgres search vectors and ranks

--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -1,6 +1,6 @@
 from django.contrib.postgres import search
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.core.mail import send_mail
@@ -115,9 +115,12 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data["query"]
+            search_vector = SearchVector("title", "body")
+            search_query = SearchQuery(query)
             results = Post.published.annotate(
-                search=SearchVector("title", "body"),
-            ).filter(search=query)
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(search=search_query).order_by("-rank")
     return render(
         request,
         "blog_app/post/search.html",
